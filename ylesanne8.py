65309f6ac74acbc8932b76962cbb6e5b6c1d7fcd
onuaxel <34282351+onuaxel@users.noreply.github.com>
2018-12-18 11:12:23 +0200

    Add files via upload
    Comparison logic changed according to leap year definition

--- ylesanne8.py
+++ ylesanne8.py
@@ -3,9 +3,12 @@
 # kas antud positiivne täisarv on liig- või lihtaasta number. 
 # (Aasta on liigaasta, kui tema number jagub neljaga, 
 # välja arvatud need aastad, mille number jagub sajaga, kuid ei jagu neljasajaga.)
+#VIGANE SELGITUS, PEAKS OLEMA Iga aasta, mis jagub neljaga, on liigaasta (kui ta samal ajal ei jagu sajaga). Kui aasta jagub sajaga, siis ta ei ole liigaasta. Aasta, mis jagub neljasajaga, on igal juhul liigaasta. (https://et.wikipedia.org/wiki/Liigaasta)
+
 
 year=int(input("Anna siva üks aasta number "))
-if (year%4==0 and year%100==0 and year%400!=0):
+if (year%4==0 and year%100!=0) or year%400==0:
     print("liig")
+
 else:
     print("liht")
